c29af1dd2887e329f72ad8a2d239f574881b2928
dy956122 <dy956122@gmail.com>
2020-04-09 14:25:29 +0800

    Unity_作業20200408_01

---
+++ ClassField/Assets/Enemy.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    /// <summary>
+    /// 怪物的血量
+    /// </summary>
+    [Header("血量"), Tooltip("怪物的血量")]
+    [Range(50, 100)]
+    public short 血量;
+
+    /// <summary>
+    /// 怪物的魔力
+    /// </summary>
+    [Header("魔力"), Tooltip("怪物的魔力")]
+    [Range(10,100)]
+    public short 魔力 = 100;
+
+    /// <summary>
+    /// 怪物的能力值
+    /// </summary>
+    [Header("能力值")]
+    [Range(1, 50.5f)]
+    public float 移動速度 = 10.5f ;
+
+    /// <summary>
+    /// 怪物的攻擊力
+    /// </summary>
+    [Range(20,200)]
+    public short 攻擊力 = 50 ;
+
+    /// <summary>
+    /// 怪物的防禦力
+    /// </summary>
+    [Range(0, 100)]
+    public short 防禦力 ;
+
+    /// <summary>
+    /// 怪物的武器
+    /// </summary>
+    [Header("裝備")]
+    public string 武器名稱;
+
+    /// <summary>
+    /// 怪物的裝備
+    /// </summary>
+    public string 裝備名稱 = "皮衣" ;
+
+    /// <summary>
+    /// 怪物是否帶有鑰匙
+    /// </summary>
+    [Header("其他資料"), Tooltip("怪物是否帶有鑰匙")]
+    public bool 是否帶有鑰匙 = false ;
+
+    /// <summary>
+    /// 怪物是否帶有寶物
+    /// </summary>
+    [Tooltip("怪物是否帶有寶物")]
+    public bool 是否掉落寶物 = false;
+}
+
---
+++ ClassField/Assets/EnemyManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    [Header("怪物一號")]
+    public Enemy 怪物一號;
+
+    [Header("怪物二號")]
+    public Enemy 怪物二號;
+
+    void Start()
+    {
+        print("怪物一號的魔力:" + 怪物一號.血量);
+        print("怪物二號的裝備名稱:" + 怪物二號.裝備名稱) ;
+
+        怪物一號.血量 = 100 ;
+        怪物二號.血量 = 300 ;
+
+        怪物一號.防禦力 = 50;
+
+        怪物一號.武器名稱 = "木棍" ;
+        怪物二號.武器名稱 = "短刀" ;
+
+        怪物一號.是否帶有鑰匙 = true ;
+        怪物二號.是否掉落寶物 = true ;
+    }
+
+}
